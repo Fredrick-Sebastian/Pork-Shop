31f9b7c1a454ea371e778f03b996a280aaddea3e
Fredrick Sebastian <futurefamily154@gmail.com>
2025-08-02 16:08:59 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// Select toggle button and nav links
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+// Add click event to toggle the menu
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
